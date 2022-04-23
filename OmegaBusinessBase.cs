e7d052a09fd99637d0ab68181884ba8bed3d772c
Binesh George <bineshgeorge@gmail.com>
2022-04-23 20:57:23 +0800

    First Commit

---
+++ OmegaBusinessBase.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Omega.Core;
+using Omega.Core.Interface;
+using Omega.Data;
+
+namespace Omega.ServiceBase
+{
+    public abstract class OmegaBusinessBase<T, TF> : OmegaServiceBase
+        where T : IDataModel, new()
+        where TF : IModelFilter , new()
+    { 
+
+        public OmegaBusinessBase(DataSession dataSession) : base(dataSession)
+        {
+
+        }
+        
+
+        ~OmegaBusinessBase()
+        {
+            Dispose(false);
+        }
+        
+
+        public virtual async Task<List<T>> Fetch(TF filter)
+        {
+            var tcs = new TaskCompletionSource<List<T>>();
+            tcs.SetException(new NotImplementedException());
+            return await tcs.Task;
+            //return await Task.Run(() => new List<T>());
+        }
+        public async Task<T?> FetchOne(TF filter)
+        {
+            var data = await Fetch(filter);
+            if (data == null || data.Count <= 0 ) return default;
+            return data[0];
+        }
+        public virtual async Task<bool> Add(T model)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            tcs.SetException(new NotImplementedException());
+            return await tcs.Task;
+        }
+
+        public virtual async Task<bool> Update(T model)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            tcs.SetException(new NotImplementedException());
+            return await tcs.Task;
+        }
+
+        public virtual async Task<bool> Validate(T model)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            tcs.SetException(new NotImplementedException());
+            return await tcs.Task;
+        }
+        public virtual async Task<bool> Delete(T model)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            tcs.SetException(new NotImplementedException());
+            return await tcs.Task;
+        }
+
+      
+    }
+}
---
+++ OmegaServiceBase.cs
@@ -0,0 +1,122 @@
+﻿using Omega.Core;
+using Omega.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omega.ServiceBase
+{
+    public abstract class OmegaServiceBase : IDisposable
+    {
+        private readonly DataSession _dataSession;
+        private List<vMessageData> _messgeList;
+
+
+        public OmegaServiceBase(DataSession dataSession)
+        {
+            _dataSession = dataSession;
+            _messgeList = new List<vMessageData>();
+        }
+
+        public string NetworkID { get; set; }
+        public string UserName { get; set; }
+        public vLanguageCode LanguageCode { get; set; } = vLanguageCode.English;
+        public short CountryCode { get; set; }
+
+
+        #region IDisposable
+        public virtual void ReleaseUnmanagedResources()
+        {
+            // TODO release unmanaged resources here
+        }
+
+      
+        protected virtual void Dispose(bool disposing)
+        {
+            ReleaseUnmanagedResources();
+            if (disposing)
+            {
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion 
+
+        public DataSession SessionObject
+        {
+            get { return _dataSession; }
+        }
+
+        public List<vMessageData> MessgeList
+        {
+            get
+            {
+                if (_messgeList == null) _messgeList = new List<vMessageData> { };
+                return _messgeList;
+            }
+        }
+
+        public void ParseMessage(string message)
+        {
+            var data = message.Split(';', StringSplitOptions.RemoveEmptyEntries);
+            this.MessgeList.Add(new vMessageData()
+            {
+                Code = Convert.ToInt16(data[0]),
+                Message = data[1].ToString()
+            }) ;
+        }
+        public int RowToInt(object val)
+        {
+            int retVal = 0;
+            if (val is DBNull || val == DBNull.Value || val == null) return retVal;
+            int.TryParse(val.ToString(), out retVal);
+            return retVal;
+        }
+
+        public short RowToShort(object val)
+        {
+            short retVal = 0;
+            if (val is DBNull || val == DBNull.Value || val == null) return retVal;
+            short.TryParse(val.ToString(), out retVal);
+            return retVal;
+        }
+
+        public double RowToDouble(object val)
+        {
+            double retVal = 0D;
+            if (val is DBNull || val == DBNull.Value || val == null) return retVal;
+            double.TryParse(val.ToString(), out retVal);
+            return retVal;
+        }
+
+        public decimal RowToDecimal (object val)
+        {
+            decimal retVal = 0;
+            if (val is DBNull || val == DBNull.Value || val == null) return retVal;
+            decimal.TryParse(val.ToString(), out retVal);
+            return retVal;
+        }
+
+        public bool RowToBoolean(object val)
+        {
+            bool retVal = false;
+            if (val is DBNull || val == DBNull.Value || val == null) return retVal;
+            bool.TryParse(val.ToString(), out retVal);
+            return retVal;
+        }
+        public bool IsReturnSuccess(object val)
+        {
+            int result = 5100; // false
+            if (val == null || val == DBNull.Value) return false;
+            int.TryParse(val.ToString(), out result);
+            return (result == vConstant.C_RET_SUCCESS);
+        }
+    }
+}
